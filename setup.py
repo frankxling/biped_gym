becfc66589b04403326d0062b4d8d1fda8d66f77
pohzhiee <poh140996@hotmail.com>
2019-10-09 14:00:29 +0800

    update setup requirement

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='biped_gym',
       version='0.1',
       description='Biped environment for OpenAI gym',
-      install_requires=['gym']
+      install_requires=['gym', 'billiard']
 )
